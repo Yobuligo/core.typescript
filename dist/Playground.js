3fda4b5d196e2b42fb07a6d0195fe1ce13a909e6
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-17 01:20:40 +0100

    Update version

--- dist/Playground.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var src_1 = require("./src");
-var Exceptions_1 = require("./src/Exceptions");
-(0, src_1.error)(new Exceptions_1.NullPointerException("With message"));
-//# sourceMappingURL=Playground.js.map
